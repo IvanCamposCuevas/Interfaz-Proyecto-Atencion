14e6b5d67068a1ab3aa16071edafe541e9a2776b
IvanCamposCuevas <iva.campos.cuevas@gmail.com>
2019-10-12 22:04:09 -0300

    Interfaz Web del portal de atenciones medicas, montado en Angular 8 y Bootstrap

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
+
+
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AtencionComponent } from './atencion/atencion.component';
+import { NumeroComponent } from './numero/numero.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent,
+    HomeComponent,
+    AtencionComponent,
+    NumeroComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
